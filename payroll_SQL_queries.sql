6f57794cc85f22442cde7c3e02ae977c65824a40
LokeshKiran <lokeshkskiran@gmail.com>
2022-10-15 16:29:42 +0530

    [Lokesh]Added:inserting rows into the table

--- payroll_SQL_queries.sql
+++ payroll_SQL_queries.sql
@@ -1,10 +1,21 @@
 create database employee_payroll_service
 use employee_payroll_service
 
+<<<<<<< HEAD
 create table employee_payroll_service
+=======
+create table employee_payroll
+>>>>>>> UC2_CreateTable
 (
  id int identity(1,1) primary key,
  name varchar(100) not null,
  salary money not null,
  start date not null
 );
+
+select * from employee_payroll
+
+insert into employee_payroll values
+('Billi',100000.0,'2018-01-03'),
+('Terisa',200000.0,'2019-11-13'),
+('Charlie',300000.0,'2021-05-21')
