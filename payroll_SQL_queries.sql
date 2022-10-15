379bd7c4828947df56f457c4cf42f0043cff8396
LokeshKiran <lokeshkskiran@gmail.com>
2022-10-15 16:13:13 +0530

    [Lokesh]Added:creating a new table

--- payroll_SQL_queries.sql
+++ payroll_SQL_queries.sql
@@ -1,2 +1,10 @@
 create database employee_payroll_service
 use employee_payroll_service
+
+create table employee_payroll_service
+(
+ id int identity(1,1) primary key,
+ name varchar(100) not null,
+ salary money not null,
+ start date not null
+);
